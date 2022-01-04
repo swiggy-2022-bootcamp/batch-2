40fb9ca8df654cf96ff3a2e8da0a8642e86fb793
Nikhilnama18 <nikhilnama1111@gmail.com>
2022-01-04 19:02:41 +0530

    Day-1

---
+++ LearnJs.js
@@ -0,0 +1,28 @@
+let One = 3;
+let Two = 3;
+
+console.log("One == Two ", One == Two);
+
+let js = "JavaScript";
+let cpp = "C++";
+
+console.log(js == cpp);
+
+let n = [1, 2, 3];
+let nums = [1, 2, 3];
+
+console.log("n==nums ", n == nums);
+
+let user1 = {
+  name: "Nick",
+  role: "Student",
+  country: "Ind",
+};
+
+let user2 = {
+  name: "Pushpa",
+  role: "Faculty",
+  country: "Ind",
+};
+
+console.log("User 1 == User 2", user1 == user2);
