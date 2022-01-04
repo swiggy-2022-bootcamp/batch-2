cfbdd329035dc2a27207dfe335fe613f5197fb9a
Piyush Sehgal <piyush97sehgal@gmail.com>
2022-01-04 18:50:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let name = 'Piyush Sehgal';
+let name2 = 'Piyush Sehgal';
+
+let age = 32;
+let age2 = 32;
+
+let person = {
+    name: 'Ajay',
+    age: 45,
+    hobbies: ['singing', 'video games']
+}
+
+let person2 = {
+    name: 'Ajay',
+    age: 45,
+    hobbies: ['singing', 'video games']
+}
+
+let PI = 3.14;
+
+name += " Appended string"
+
+console.log(name, age, person.name, person.age, person.hobbies, PI);
+console.log(typeof name, typeof person, typeof PI)
+
+console.log('primitive comparison: ', age == age2)
+console.log('Objects comparison: ', person == person2)
+
+const num = Math.floor(Math.random () * 100000)
+console.log(num);
+
