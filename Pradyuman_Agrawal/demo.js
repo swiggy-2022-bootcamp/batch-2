74223ad88f8276fb5cb1581f8c589947677034c6
PradyumanAgrawal <pa14@iitbbs.ac.in>
2022-01-04 19:05:49 +0530

    Demo commit - Day 1

---
+++ Pradyuman_Agrawal/demo.js
@@ -0,0 +1,23 @@
+let string = "JavasCript"
+console.log(string)    
+
+string="hello"
+console.log(string)
+
+string[0]="a"
+console.log(string)
+
+
+let a=10;
+console.log(a)
+a=5
+console.log(a)
+
+
+const num = Math.floor(Math.random () * 101)
+console.log(num)
+
+const now = new Date()
+console.log(now)
+console.log(now.getTime())
+
