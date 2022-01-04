ab4d59ed110c67740d5d1a8318300770ac9c8604
codescientist703 <nihalmittal47@gmail.com>
2022-01-04 19:09:13 +0530

    init: Nihal Mittal Day 1

---
+++ Nihal Mittal/Day1/demo.js
@@ -0,0 +1,21 @@
+let nums = [1, 2, 3];
+let numbers = [1, 2, 3];
+
+console.log(nums == numbers); // false
+
+let userOne = {
+	name: "Asabeneh",
+	role: "teaching",
+	country: "Finland",
+};
+
+let userTwo = {
+	name: "Asabeneh",
+	role: "teaching",
+	country: "Finland",
+};
+
+let ha = 1;
+let ha2 = 1;
+console.log(userOne == userTwo);
+console.log(ha == ha2);
