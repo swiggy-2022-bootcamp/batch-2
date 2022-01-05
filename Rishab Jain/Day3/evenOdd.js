51858d6c993c09713852d11f139c3c8ac239d751
Rishab Jain <rjrishabh.jain5@gmail.com>
2022-01-05 21:41:26 +0530

    Practice day 3

---
+++ Rishab Jain/Day3/evenOdd.js
@@ -0,0 +1,6 @@
+let evenOdd = (number) => (number % 2 == 0);
+
+console.log(evenOdd(2));
+console.log(evenOdd(7));
+console.log(evenOdd(70));
+console.log(evenOdd(68));
---
+++ Rishab Jain/Day3/maps.js
@@ -0,0 +1,5 @@
+const fruits = new Map();
+
+fruits.set("apples", 500);
+fruits.set("bananas", 300);
+fruits.set("oranges", 200);
