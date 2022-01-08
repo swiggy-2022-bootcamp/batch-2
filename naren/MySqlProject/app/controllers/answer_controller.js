a938f1159003d871f9a4064ba2a534d031e3e281
narenq7 <narencr7@gmail.com>
2022-01-08 16:27:04 +0530

    added authentication for all tasks

--- naren/MySqlProject/app/controllers/answer_controller.js
+++ naren/MySqlProject/app/controllers/answer_controller.js
@@ -1,7 +1,7 @@
 const sql = require("./db.js")
 
 
-//testing inserting an answer into answers table
+// API to post answer
 const postAnswer = (req,res) =>
 {
     const answersData =
@@ -15,23 +15,37 @@ const postAnswer = (req,res) =>
         userName : req.body.user_Details.userName,
         password : req.body.user_Details.password
     }
+
     // lets do the authentication part later
-    const queryString = 'INSERT INTO Answers SET ?;';
-    sql.query(queryString,answersData,(err,result) => 
+    const authenticateQueryString = 'SELECT * FROM UserDetails where userName = ? and password =?;';
+    sql.query(authenticateQueryString,[userData.userName,userData.password],(err,result) => 
     {
-        if(err)
+        if(err ||result.length == 0 )
         {
-            console.log("error:maybe check for valid QUESTION ID OR IF user has already answered this question before. then try a PUT REQUEST TO update the answer",err);
-            res.status(400).send({ message: 'This is an error! maybe check for valid QUESTION ID OR IF user has already answered this question before. then try a PUT REQUEST TO update the answer'});
+            console.log("Error: INVALID CREDENTIALS FOR LOGIN. PLEASE TRY AGAIN");
+            res.status(401).send({ message: "Error: INVALID CREDENTIALS FOR LOGIN. PLEASE TRY AGAIN"});
         }
         else
         {
-            console.log("Answer for question "+answersData.questionId+" posted Successfully with ID "+result.insertId);
-            res.status(201).send({message:"Answer for question "+answersData.questionId+" posted Successfully with ID "+result.insertId});
+            const queryString = 'INSERT INTO Answers SET ?;';
+            sql.query(queryString,answersData,(err,result) => 
+            {
+                if(err)
+                {
+                    console.log("error:maybe check for valid QUESTION ID OR IF user has already answered this question before. then try a PUT REQUEST TO update the answer",err);
+                    res.status(400).send({ message: 'This is an error! maybe check for valid QUESTION ID OR IF user has already answered this question before. then try a PUT REQUEST TO update the answer'});
+                }
+                else
+                {
+                    console.log("Answer for question "+answersData.questionId+" posted Successfully with ID "+result.insertId);
+                    res.status(201).send({message:"Answer for question "+answersData.questionId+" posted Successfully with ID "+result.insertId});
+                }
+            })
         }
     })
-}
+}   
 
+// API to update answer
 const updateAnswer = (req,res) =>
 {
     const answersData =
@@ -45,48 +59,47 @@ const updateAnswer = (req,res) =>
         userName : req.body.user_Details.userName,
         password : req.body.user_Details.password
     }
-
-    const queryString = 'SELECT * FROM Answers where userName = ? and questionId = ?;';
-    sql.query(queryString,[answersData.userName,answersData.questionId],(err,result) => 
+    const authenticateQueryString = 'SELECT * FROM UserDetails where userName = ? and password =?;';
+    sql.query(authenticateQueryString,[userData.userName,userData.password],(err,result) => 
     {
-        if(err)
+        if(err ||result.length == 0 )
         {
-            console.log("error: ",err)
-            res.status(400).send({ message: 'This is an error!'});
+            console.log("Error: INVALID CREDENTIALS FOR LOGIN. PLEASE TRY AGAIN");
+            res.status(401).send({ message: "Error: INVALID CREDENTIALS FOR LOGIN. PLEASE TRY AGAIN"});
         }
         else
         {
-            result = JSON.parse(JSON.stringify(result));
-            //console.log(result.length);
-            if (result.length == 1)
+            const queryString = 'SELECT * FROM Answers where userName = ? and questionId = ?;';
+            sql.query(queryString,[answersData.userName,answersData.questionId],(err,result) => 
             {
-                // USER has answered this question
-                // lets update the answer with new content.
-                const queryString2 = 'update Answers SET content = ? where userName =? and questionId =?;';
-                sql.query(queryString2,[answersData.content,answersData.userName,answersData.questionId],(err,result) => 
+                if(err || result.length == 0)
                 {
-                    if(err)
+                    console.log("User has NOT YET answered this question. please raise a POST REQUEST to add answer");
+                    res.status(403).send({ message: "User has NOT YET answered this question. please raise a POST REQUEST to add answer"});   
+                }
+                else
+                {
+                    // USER has answered this question
+                    // lets update the answer with new content.
+                    const queryString2 = 'update Answers SET content = ? where userName =? and questionId =?;';
+                    sql.query(queryString2,[answersData.content,answersData.userName,answersData.questionId],(err,result) => 
                     {
-                        console.log("error: ",err)
-                        res.status(400).send({ message: 'This is an error!'});
-                    }
-                    else
-                    {
-                        console.log("Answer for question "+answersData.questionId+" updated SUCCESSFULLY");
-                        res.status(200).send({message:"Answer for question "+answersData.questionId+" updated SUCCESSFULLY"});
-                    }
-                });
-            }    
-            else
-            {
-                console.log("User has NOT YET answered this question. please raise a POST REQUEST to add answer");
-                res.status(403).send({ message: "User has NOT YET answered this question. please raise a POST REQUEST to add answer"});
-            }       
+                        if(err)
+                        {
+                            console.log("error: ",err)
+                            res.status(400).send({ message: 'This is an error!'});
+                        }
+                        else
+                        {
+                            console.log("Answer for question "+answersData.questionId+" updated SUCCESSFULLY");
+                            res.status(200).send({message:"Answer for question "+answersData.questionId+" updated SUCCESSFULLY"});
+                        }
+                    });
+                }    
+            });   
         }
-    });   
+    })
 }
-
-
 
 
 module.exports ={postAnswer,updateAnswer}
--- naren/MySqlProject/app/controllers/question_controller.js
+++ naren/MySqlProject/app/controllers/question_controller.js
@@ -4,6 +4,12 @@ const sql = require("./db.js")
 const getAllAnswersForQuestion = (req,res) =>
 {
     
+    const userData=
+    {       
+        userName : req.body.userName,
+        password : req.body.password
+    }
+
     const displayObject =
     {
         "Question" : "",
@@ -11,43 +17,55 @@ const getAllAnswersForQuestion = (req,res) =>
     }
     
     const questionId = req.params.id;
-    const queryString = 'select content from Questions where questionId = ?'
-    
-    sql.query(queryString,questionId,(err,result) => 
+
+    const authenticateQueryString = 'SELECT * FROM UserDetails where userName = ? and password =?;';
+    sql.query(authenticateQueryString,[userData.userName,userData.password],(err,result) => 
     {
-        if((err) || result.length == 0)
+        if(err ||result.length == 0 )
         {
-            console.log("error: INVALID QUESTION ID ")
-            res.status(404).send({ message: 'This is an error! INVALID QUESTION ID'});
+            console.log("Error: INVALID CREDENTIALS FOR LOGIN. PLEASE TRY AGAIN");
+            res.status(401).send({ message: "Error: INVALID CREDENTIALS FOR LOGIN. PLEASE TRY AGAIN"});
         }
         else
         {
-            displayObject.Question = result[0].content;
-            const queryString2 = 'select content from Answers where questionId = ? order by answerId;'
-            sql.query(queryString2,questionId,(err,result) => 
+            const queryString = 'select content from Questions where questionId = ?'
+    
+            sql.query(queryString,questionId,(err,result) => 
             {
-                if(err)
+                if((err) || result.length == 0)
                 {
-                    console.log("error: ",err)
-                    res.status(404).send({ message: 'This is an error!'});
+                    console.log("error: INVALID QUESTION ID ")
+                    res.status(404).send({ message: 'This is an error! INVALID QUESTION ID'});
                 }
                 else
                 {
-                    result = JSON.parse(JSON.stringify(result));
-
-                    for (var index =0;index<result.length;index++)
+                    displayObject.Question = result[0].content;
+                    const queryString2 = 'select content from Answers where questionId = ? order by answerId;'
+                    sql.query(queryString2,questionId,(err,result) => 
                     {
-                        displayObject.Answers.push(result[index].content);
-                    }
-                    console.log(JSON.stringify(displayObject));
-                    res.status(200).send(JSON.stringify(displayObject));
-                }       
+                        if(err)
+                        {
+                            console.log("error: ",err)
+                            res.status(404).send({ message: 'This is an error!'});
+                        }
+                        else
+                        {
+                            result = JSON.parse(JSON.stringify(result));
+
+                            for (var index =0;index<result.length;index++)
+                            {
+                                displayObject.Answers.push(result[index].content);
+                            }
+                            console.log(JSON.stringify(displayObject));
+                            res.status(200).send(JSON.stringify(displayObject));
+                        }       
        
+                    });
+                }
             });
         }
-    });
-}
-
+    })
+};
 
 
 //testing inserting a question into questions table
@@ -64,20 +82,33 @@ const postQuestion = (req,res) =>
         password : req.body.user_Details.password
     }
     // lets do the authentication part later
-    const queryString = 'INSERT INTO Questions SET ?;';
-    sql.query(queryString,questionData,(err,result) => 
+
+    const authenticateQueryString = 'SELECT * FROM UserDetails where userName = ? and password =?;';
+    sql.query(authenticateQueryString,[userData.userName,userData.password],(err,result) => 
     {
-        if(err)
+        if(err ||result.length == 0 )
         {
-            console.log("error: ",err)
-            res.status(400).send({ message: 'This is an error!'});
+            console.log("Error: INVALID CREDENTIALS FOR LOGIN. PLEASE TRY AGAIN");
+            res.status(401).send({ message: "Error: INVALID CREDENTIALS FOR LOGIN. PLEASE TRY AGAIN"});
         }
         else
         {
-            console.log("Question posted Successfully with ID "+result.insertId);
-            res.status(201).send({message:"Question posted Successfully with ID "+result.insertId});
+            const queryString = 'INSERT INTO Questions SET ?;';
+            sql.query(queryString,questionData,(err,result) => 
+            {
+                if(err)
+                {
+                    console.log("error: CHECK type of username and content ",err)
+                    res.status(400).send({ message: 'ERROR: CHECK type of username and content'});
+                }
+                else
+                {
+                    console.log("Question posted Successfully with ID "+result.insertId);
+                    res.status(201).send({message:"Question posted Successfully with ID "+result.insertId});
+                }
+            })
         }
-    })
+    });
 }
 
 module.exports ={getAllAnswersForQuestion,postQuestion}
