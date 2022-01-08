028697cb9a1a43849db7ddc5e9bd01cb3344deb9
narenq7 <narencr7@gmail.com>
2022-01-08 10:51:30 +0530

    added user model

---
+++ naren/MySqlProject/app/models/user_model.js
@@ -0,0 +1,10 @@
+//const sql = require("./db.js")
+
+
+// COULD NOT IMPLEMENT NESTED QUERIES BECAUSE OF ASYNCH NATURE OF NODE JS
+// when i separate the express middleware's rest api and Database query functionality,
+// sometimes the response of the request happens before my query completion
+// because of asynch nature. so for the case of this project, i have combined
+// the model and controller. For future work, ill learn and make it modular
+// with MVC STYLE.
+
