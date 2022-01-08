2448f075d51a38e20d978138658d0dd80f370dfc
Pradeep Singh <pradeep.singh@zestmoney.in>
2022-01-09 03:30:33 +0530

    added middleware named protect to authenticate all incoming requests

--- stackoverflow.com/src/utils/auth.js
+++ stackoverflow.com/src/utils/auth.js
@@ -61,3 +61,30 @@ export const signin = async (req, res) => {
     res.status(500).end();
   }
 };
+
+export const protect = async (req, res, next) => {
+    const bearer = req.headers.authorization;
+    if (!bearer || !bearer.startsWith("Bearer ")) {
+      return res.status(401).end();
+    }
+  
+    const token = bearer.split("Bearer ")[1].trim();
+    let payload;
+    try {
+      payload = await verifyToken(token);
+    } catch (e) {
+      return res.status(401).end();
+    }
+  
+    const user = await User.findById(payload.id)
+      .select("-password")
+      .lean()
+      .exec();
+  
+    if (!user) {
+      return res.status(401).end();
+    }
+  
+    req.user = user;
+    next();
+  };
