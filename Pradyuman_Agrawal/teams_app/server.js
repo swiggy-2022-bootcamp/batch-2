43d0ebe762a6e2fe223aac0162421848bc9051dc
PradyumanAgrawal <pa14@iitbbs.ac.in>
2022-01-08 16:07:57 +0530

    Request logger and unknown error endpoints handler implemented with middleware

--- Pradyuman_Agrawal/teams_app/server.js
+++ Pradyuman_Agrawal/teams_app/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const middleware_utils = require('./middleware/utils');
 
 const app = express();
 
@@ -10,12 +11,17 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(middleware_utils.requestLogger);
 
 app.get("/test",(req,res) => {
     res.json({message:"welcome to NodeJS App"})
 });
 
 require("./routes/users")(app);
+
+app.use(middleware_utils.unknownEndpoint);
+app.use(middleware_utils.errorHandler);
+
 
 const PORT = process.env.PORT||3000
 app.listen(PORT,() => {
