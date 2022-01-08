66715167a10236f48e7387ef8a4f481d7272aae0
Pradeep Singh <pradeep.singh@zestmoney.in>
2022-01-09 03:34:33 +0530

    create reusable updateOne createOne and deleteOne method for all models

--- stackoverflow.com/src/utils/crud.js
+++ stackoverflow.com/src/utils/crud.js
@@ -25,3 +25,61 @@ export const getMany = (model) => async (req, res) => {
     res.status(400).end();
   }
 };
+
+export const createOne = (model) => async (req, res) => {
+  const createdBy = req.user._id;
+  try {
+    const document = await model.create({ ...req.body, createdBy });
+    res.status(201).json({ data: document });
+  } catch (e) {
+    console.error(e);
+    res.status(400).end();
+  }
+};
+
+export const updateOne = (model) => async (req, res) => {
+  try {
+    const updatedDocument = await model
+      .findOneAndUpdate(
+        {
+          createdBy: req.user._id,
+          _id: req.params.id,
+        },
+        req.body,
+        { new: true }
+      )
+      .lean()
+      .exec();
+
+    if (!updatedDocument) {
+      return res.status(400).end();
+    }
+
+    res.status(200).json({ data: updatedDocument });
+  } catch (err) {
+    res.status(400).end();
+  }
+};
+
+export const deleteOne = (model) => async (req, res) => {
+  try {
+    const removed = await model.findOneAndRemove({
+      createdBy: req.user._id,
+      _id: req.params.id,
+    });
+    if (!removed) {
+      return res.status(400).end();
+    }
+    return res.status(200).json({ data: removed });
+  } catch (err) {
+    res.status(400).end();
+  }
+};
+
+export const crudRepository = (model) => ({
+  createOne: createOne(model),
+  findOne: getOne(model),
+  findMany: getMany(model),
+  updateOne: updateOne(model),
+  removeOne: deleteOne(model),
+});
