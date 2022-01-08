2d7ef4197b6a0120e13ac49738891d55aecc1e7e
Rahul Panchal <rahulnpanchal50@gmail.com>
2022-01-08 16:10:23 +0530

    Express, DB Connection & Project Structure Setup

---
+++ Meeting-App/app.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: __dirname + '/.env' });
+require('./src/config/dbConn');
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Meeting Application');
+});
+
+app.listen(PORT, () => {
+  	console.log(`Meeting App Running on Port: ${PORT} (http://localhost:${PORT})`);
+});
