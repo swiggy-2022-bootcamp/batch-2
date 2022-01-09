8d7523ca8bf4389ea84d6761448f1376975ec000
Mradul Rathore <er.mradulrathore@gmail.com>
2022-01-09 22:22:31 +0530

    authenticate user endpoint

--- Mradul-Rathore-Food-App-Case-Study/app/controllers/user.controller.js
+++ Mradul-Rathore-Food-App-Case-Study/app/controllers/user.controller.js
@@ -52,7 +52,42 @@ exports.registerUser = async (req, res) => {
     }
 };
 
+//authenticate user
+exports.authenticate = async (req, res) => {
+    // Our login logic starts here
+    try {
+        // Get user input
+        const { email, password } = req.body;
 
+        // Validate user input
+        if (!(email && password)) {
+            res.status(400).send("All input is required");
+        }
+        // Validate if user exist in our database
+        const user = await User.findOne({ email });
 
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                }
+            );
 
+            // save user token
+            user.token = token;
+
+            const sucessMessage = {
+                message: "user logged in successful"
+            }
+            // user
+            res.status(200).json(sucessMessage);
+        }
+        res.status(403).send("Forbidden");
+    } catch (err) {
+        console.log(err);
+    }
+}
 
