eead583e38f218813615972337dd83f2721a491c
narenq7 <narencr7@gmail.com>
2022-01-09 15:46:37 +0530

    changed user controller to async

--- naren/MySqlProject/app/routes/answer_routes.js
+++ naren/MySqlProject/app/routes/answer_routes.js
@@ -1,15 +1,18 @@
+
+// This defines the endpoints used to respond to requests related to answers.
+
 module.exports = app => 
 {
-    const answers = require("../controllers/answer_controller.js");
-    var router = require("express").Router();
-
-    //router.get("/test",questions.testquestion);
-    router.post("/addAnswer",answers.postAnswer);
-    router.put("/updateAnswer",answers.updateAnswer);
-    router.post("/upvoteAnswer/:id",answers.upvoteAnswer);
-    router.get("/getPrivilegedUsers",answers.getPrivilegedUsers);
-    router.get("/upvoteCount",answers.getVoteCountOfAnswers);
-    router.delete("/deleteAnswer/:id",answers.deleteAnswer);
- 
+    const answers = require("../controllers/test_answer_controller.js");
+    const router = require("express").Router();
+    
+    router.put("/updateAnswer", answers.updateAnswer);
+    /*
+    router.post("/addAnswer", answers.postAnswer);
+    router.post("/upvoteAnswer/:id", answers.upvoteAnswer);
+    router.get("/getPrivilegedUsers", answers.getPrivilegedUsers);
+    router.get("/upvoteCount", answers.getVoteCountOfAnswers);
+    router.delete("/deleteAnswer/:id", answers.deleteAnswer);
+    */
     app.use("/answers",router)
 }
--- naren/MySqlProject/app/routes/user_routes.js
+++ naren/MySqlProject/app/routes/user_routes.js
@@ -1,11 +1,11 @@
-module.exports = app => {
+// This defines the endpoints used to respond to requests related to users.
+
+module.exports = app => 
+{
     const users = require("../controllers/test_user_controller.js");
     var router = require("express").Router();
-
     router.post("/registerUser",users.createUser);
-    //router.get("/listAllUsers",users.listAllUsers)
-    //router.post("/login",users.loginUser)
-    
-
+    router.get("/listAllUsers",users.listAllUsers)
+    router.post("/login",users.loginUser)
     app.use("/users",router)
 }
