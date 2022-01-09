66cca1b2510fb5a2cb4b8fb74ba00b0e2c003239
Mradul Rathore <er.mradulrathore@gmail.com>
2022-01-09 22:25:12 +0530

    update user by id endpoint

--- Mradul-Rathore-Food-App-Case-Study/app/controllers/user.controller.js
+++ Mradul-Rathore-Food-App-Case-Study/app/controllers/user.controller.js
@@ -137,3 +137,20 @@ exports.updateUserById = (req, res) => {
     })
 }
 
+//delete user by id
+exports.deleteUserById = (req, res) => {
+    const id = req.params.id;
+    User.findByIdAndRemove(id, { useFindAndModify: false }).then(
+        data => {
+            if (!data)
+                res.status(404).send({ message: "Sorry user with " + id + " not found" });
+            else
+                res.send({ message: "User deleted successfully" });
+        }
+    ).catch(err => {
+        res.status(500).send({
+            message: err.message || "error deleting the user with id " + id
+        })
+    })
+
+}
