1b30879c95bd2402e059d6335c48b903c3dc7a31
Rahul Panchal <rahulnpanchal50@gmail.com>
2022-01-10 01:00:01 +0530

    Added README & Postman Collection File

--- Meeting-App/src/middleware/authenticate-token.js
+++ Meeting-App/src/middleware/authenticate-token.js
@@ -5,12 +5,16 @@ const authenticateToken = (req, res, next) => {
     const bearerToken = authHeader && authHeader.split(' ')[1];
 
     if(bearerToken == null){
-        return res.sendStatus(401);
+        return res.status(401).json({
+            errorMessage: `Unauthorize Request!!!. Please provide authentication credentials`
+        });
     }
 
     jwt.verify(bearerToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if(err) {
-            return res.sendStatus(403);
+            return res.status(403).json({
+                errorMessage: `Please provide Valid authentication credentials`
+            });
         }
         req.user = user;
         next();
